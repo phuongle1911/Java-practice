eabfc2f434c35d34eacb9a194b5311804e6792cc
Phuong Le <82745747+phuongle1911@users.noreply.github.com>
2026-05-18 22:58:28 +1000

    fix: move .gitignore file

--- homework2/homework2_4.java
+++ homework2/homework2_4.java
@@ -2,9 +2,18 @@ package homework2;
 
 public class homework2_4 {
   public int singleNumber(int[] nums) {
-    for (i=0; i< nums.length; i++) {
-      if nums[i] == 
-    }
+    int counter = 0;
+    for (int n=1; n<= nums.length; n++) {
+      for (int i=0; i<nums.length; i++) {
+        if (nums[i] == n) {
+          counter += 1;
+        }
+      };
+
+      if (counter == 1) {
+        return n;
+      }
+    };
   }
   
 }
