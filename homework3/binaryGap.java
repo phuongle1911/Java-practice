c50d78daa9d1995447fa598ece629527fff7da18
Phuong Le <82745747+phuongle1911@users.noreply.github.com>
2026-05-23 10:03:07 +1000

    feat: completed binary Gap counting exercise- homework3

---
+++ homework3/binaryGap.java
@@ -0,0 +1,41 @@
+package homework3;
+import java.util.ArrayList;
+
+class Solution {
+  public static int solution(int N) {
+    String binary = Integer.toBinaryString(N);
+    System.out.println("binary of num: " + binary);
+    int maxGap = 0;
+    // int[] positionOf1 = {};
+    // int n = 0;
+    ArrayList<Integer> positionOf1 = new ArrayList<Integer>();
+
+    for (int i=0; i<binary.length();i++) {
+      if (binary.charAt(i) == '1') {
+        positionOf1.add(i);
+      };
+    };
+
+    System.out.println("position of 1: " + positionOf1);
+
+    if (positionOf1.size() >=2) {
+      for (int i=0; i<(positionOf1.size()-1); i++) {
+        if (positionOf1.get(i+1) - positionOf1.get(i) - 1 > maxGap) {
+          maxGap = positionOf1.get(i+1) - positionOf1.get(i) - 1;
+        }
+      }
+    } else {
+      maxGap = 0;
+    };
+
+    return maxGap;
+  }
+
+  public static void main(String[] args) {
+    int num = 7;
+    System.out.println(solution(num));
+  }
+
+}
+
+
