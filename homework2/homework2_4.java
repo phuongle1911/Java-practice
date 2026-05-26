8121abacdde7c90e66305e43799fd1f4f409cbab
Phuong Le <82745747+phuongle1911@users.noreply.github.com>
2026-05-26 22:22:25 +1000

    feat: added test case for homework2_4

--- homework2/homework2_4.java
+++ homework2/homework2_4.java
@@ -1,13 +1,18 @@
 package homework2;
 
 public class homework2_4 {
-  public int singleNumber(int[] nums) {
+  public static int singleNumber(int[] nums) {
     int single = 0;
     for (int num : nums) {
       single = single ^ num;
     }
     return single;
 
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1,2,5,5,2};
+    System.out.println(singleNumber(nums));
   }
   
 }
--- homework3/arrayDeletion.java
+++ homework3/arrayDeletion.java
@@ -1,7 +1,53 @@
 package homework3;
 
+import java.util.ArrayList;
+
 class arrayDeletion {
   public static int solution(int[] A) {
-    
+    // number of num at even index = number of num at odd index + 1
+    // num at even index need to be highest, num at odd index need to be lowest
+    // compare each number in array with number next to it
+    // if even index number <= odd index number next to it --> delete even index number
+    // else: --> no change
+    // if odd index number < even index number next to it --> no change
+    // else: delete odd index number
+
+    int i = 0;
+    int maxS;
+    int sumOdd = 0;
+    int sumEven = 0;
+    ArrayList<Integer> newArr = new ArrayList<Integer>();
+    while (i < A.length) {
+      if (i % 2 == 0) {   // number at even index
+        if (A[i] <= A[i+1]) {
+          i += 1;
+
+        } else {
+          newArr.add(A[i]);
+          sumEven += A[i];
+          i += 1;
+          
+        }
+      } else {  // number of odd index
+        if (A[i] >= A[i+1]) {
+          // remove A[i]
+        } else {
+          i += 1;
+        }
+      }
+    }
+
+    for (int x=0; x < A.length; x+2) {
+      sumEven += A[x];
+      sumOdd += A[x+1];
+
+      maxS = sumEven - sumOdd;
+    }
+    return maxS;
   }
+
+  public static void main(String[] args) {
+    A = {}
+  }
+
 }
